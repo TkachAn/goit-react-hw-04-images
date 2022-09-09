f13501d6f43efcc7abc4d862fe9f8b6c93c4f922
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-09 21:13:45 +0300

    Update Loader.js

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,4 +1,4 @@
-import { ThreeDots } from 'react-loader-spinner'; ///dist/loader/ThreeDots
+import { ThreeDots } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
 function Loader() {
