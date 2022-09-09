25e3480528ea77dcede258596bef24c3560f928a
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-09 13:07:53 +0300

    Update Searchbar.js

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,10 +1,30 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import css from './Searchbar.module.css';
 
-function Searchbar({ onHandleSubmit, onSearchQueryChange, value }) {
+function Searchbar({ onSearch, findImageId }) {
+  const [formQuery, setFormQuery] = useState('');
+  /**/
+  const handleChange = e => {
+    if (e.target.value === '') {
+      findImageId('2649311');
+      setFormQuery('');
+    } else {
+      findImageId('2840235');
+      setFormQuery(e.target.value);
+    }
+  };
+
+  /**/
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!formQuery.trim()) return;
+    onSearch(formQuery);
+  };
+
   return (
     <header className={css.Searchbar}>
-      <form className={css.form} onSubmit={onHandleSubmit}>
+      <form className={css.form} onSubmit={handleSubmit}>
         <button type="submit" className={css.button}>
           <span className={css.label}>Search</span>
         </button>
@@ -12,11 +32,12 @@ function Searchbar({ onHandleSubmit, onSearchQueryChange, value }) {
         <input
           className={css.input}
           type="text"
-          value={value}
+          name="query"
+          value={formQuery}
+          onChange={handleChange}
           autoComplete="off"
           autoFocus
           placeholder="Search images and photos"
-          onChange={onSearchQueryChange}
         />
       </form>
     </header>
@@ -24,8 +45,8 @@ function Searchbar({ onHandleSubmit, onSearchQueryChange, value }) {
 }
 
 Searchbar.propTypes = {
-  onHandleSubmit: PropTypes.func.isRequired,
-  onSearchQueryChange: PropTypes.func.isRequired,
+  onSearch: PropTypes.func.isRequired,
+  findImageId: PropTypes.func.isRequired,
   value: PropTypes.string.isRequired,
 };
 
